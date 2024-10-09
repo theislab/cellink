c849f921738897970fa790978963f2ca66983b28
Jan Engelmann <jan.engelmann@helmholtz-munich.de>
2024-10-09 09:59:22 +0200

    add slicing by donors and cells

--- src/cellink/_core/donordata.py
+++ src/cellink/_core/donordata.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 import logging
 from dataclasses import dataclass
 
+import numpy as np
 from anndata import AnnData
 
 logger = logging.getLogger(__name__)
@@ -28,10 +31,12 @@ class DonorData:
 
     adata: AnnData
     gdata: AnnData
-    donor_key_in_sc: str
+    donor_key_in_adata: str
 
     def __post_init__(self):
         self._validate_data()
+
+        # TODO: get overlap of donors and sync data
 
     def _validate_data(self):
         """Validates that the donor key exists in the single-cell data and is categorical.
@@ -41,10 +46,12 @@ class DonorData:
             ValueError: If the donor_key_in_sc is not found
             ValueError: If adata.obs[self.donor_key_in_sc] is not categorical
         """
-        if self.donor_key_in_sc not in self.adata.obs.columns:
-            raise ValueError(f"'{self.donor_key_in_sc}' not found in adata.obs")
-        if not self.adata.obs[self.donor_key_in_sc].dtype.name == "category":
-            raise ValueError(f"'{self.donor_key_in_sc}' in adata.obs is not categorical")
+        if self.donor_key_in_adata not in self.adata.obs.columns:
+            raise ValueError(f"'{self.donor_key_in_adata}' not found in adata.obs")
+        if not self.adata.obs[self.donor_key_in_adata].dtype.name == "category":
+            raise ValueError(
+                f"'{self.donor_key_in_adata}' in adata.obs is not categorical"
+            )
 
     def get_donor_adata(self, donor: str) -> AnnData:
         """Retrieve single-cell data for a specific donor.
@@ -60,9 +67,9 @@ class DonorData:
         -------
             AnnData: AnnData object containing the donor's single-cell data.
         """
-        if donor not in self.adata.obs[self.donor_key_in_sc].cat.categories:
+        if donor not in self.adata.obs[self.donor_key_in_adata].cat.categories:
             raise ValueError(f"Donor '{donor}' not found in adata")
-        return self.adata[self.adata.obs[self.donor_key_in_sc] == donor]
+        return self.adata[self.adata.obs[self.donor_key_in_adata] == donor]
 
     def get_donor_gdata(self, donor: str) -> AnnData:
         """Retrieve genetic data for a specific donor.
@@ -81,3 +88,50 @@ class DonorData:
         if donor not in self.gdata.obs_names:
             raise ValueError(f"Donor '{donor}' not found in gdata")
         return self.gdata[donor]
+
+    def _sync_data(self, valid_donors: np.ndarray):
+        """
+        Syncs the adata and gdata objects by filtering to only include valid donors.
+
+        Args:
+            valid_donors (np.ndarray): An array of valid donor names.
+        """
+        self.adata = self.adata[
+            self.adata.obs[self.donor_key_in_adata].isin(valid_donors)
+        ]
+        self.gdata = self.gdata[self.gdata.obs_names.isin(valid_donors)]
+
+    def slice_cells(self, cell_condition) -> DonorData:
+        """
+        Returns a new DonorData object with single-cell data sliced based on the provided cell condition.
+
+        Args:
+            cell_condition: A boolean mask, condition, or slice to apply to the single-cell data.
+
+        Returns
+        -------
+            DonorData: A new DonorData object with sliced single-cell data.
+        """
+        new_adata = self.adata[cell_condition]
+        valid_donors = new_adata.obs[self.donor_key_in_adata].unique()
+        self._sync_data(valid_donors)
+
+        return DonorData(new_adata, self.gdata, self.donor_key_in_adata)
+
+    def slice_donors(self, donors: list[str]) -> DonorData:
+        """
+        Returns a new DonorData object with both single-cell and genetic data sliced to only include the specified donors.
+
+        Args:
+            donors (list[str]): A list of donor names to retain.
+
+        Returns
+        -------
+            DonorData: A new DonorData object with sliced data.
+        """
+        valid_donors = np.intersect1d(
+            self.adata.obs[self.donor_key_in_adata].unique(), donors
+        )
+        self._sync_data(valid_donors)
+
+        return DonorData(self.adata, self.gdata, self.donor_key_in_adata)
