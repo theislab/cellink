e1e1b70a9244723e8357fa2eaa429e9c2a5080d8
Jan Engelmann <jan.engelmann@helmholtz-munich.de>
2024-10-09 11:36:47 +0200

    add string representation

---
+++ tests/test_donor_data.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import pytest
+from anndata import read_h5ad
+
+from cellink._core.donordata import DonorData
+from cellink.io import read_sgkit_zarr
+
+DATA = Path("tests/data")
+
+
+@pytest.mark.slow
+def test_donordata():
+    gdata = read_sgkit_zarr(DATA / "chr22.dose.filtered.R2_0.8.vcz")
+    gdata.obs = gdata.obs.set_index("id")
+    adata = read_h5ad(
+        DATA / "debug_OneK1K_cohort_gene_expression_matrix_14_celltypes.h5ad"
+    )
+    dd = DonorData(adata, gdata, "individual")
+    print(dd)
+
+
+if __name__ == "__main__":
+    test_donordata()
