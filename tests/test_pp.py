c2e20816a4c20b3c795dc22a3c0c096184076f71
LArnoldt <github@lucas-arnoldt.de>
2024-10-10 17:15:35 +0200

    add plink files

--- tests/test_pp.py
+++ tests/test_pp.py
@@ -1,3 +1,12 @@
 from pathlib import Path
 
 import pytest
+
+DATA = Path("tests/data")
+
+@pytest.mark.slow
+def test_variant_qc():
+    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
+
+    variant_qc(gdata)
+
