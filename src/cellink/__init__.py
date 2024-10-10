38f48fca7170bf4204f1b5d19ff76d0f85ab3e9c
LArnoldt <github@lucas-arnoldt.de>
2024-10-10 11:31:59 +0200

    refactoring imports and documentation

--- src/cellink/__init__.py
+++ src/cellink/__init__.py
@@ -6,10 +6,9 @@ warnings.filterwarnings(
     message="pkg_resources is deprecated as an API",
     category=DeprecationWarning,
 )
-from . import io, pl, pp, tl  # noqa: E402
+from . import pl, pp, tl  # noqa: E402
 from ._core.donordata import DonorData
 
 __all__ = ["DonorData", "pl", "pp", "tl", "io"]
 
-
-#__version__ = version("cellink")
+__version__ = version("cellink")
