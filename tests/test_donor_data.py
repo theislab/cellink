a74fe89c68ff672390c8450b9876f26c180da531
Jan Engelmann <jan.engelmann@helmholtz-munich.de>
2024-10-10 09:33:43 +0200

    add test such that adata does not change

--- tests/test_donor_data.py
+++ tests/test_donor_data.py
@@ -13,7 +13,9 @@ DATA = Path("tests/data")
 def test_donordata_init():
     gdata = read_sgkit_zarr(DATA / "chr22.dose.filtered.R2_0.8.vcz")
     gdata.obs = gdata.obs.set_index("id")
-    adata = read_h5ad(DATA / "debug_OneK1K_cohort_gene_expression_matrix_14_celltypes.h5ad")
+    adata = read_h5ad(
+        DATA / "debug_OneK1K_cohort_gene_expression_matrix_14_celltypes.h5ad"
+    )
     dd = DonorData(adata, gdata, "individual")
     print(dd)
 
@@ -22,13 +24,17 @@ def test_donordata_init():
 def test_donordata_aggregate():
     gdata = read_sgkit_zarr(DATA / "chr22.dose.filtered.R2_0.8.vcz")
     gdata.obs = gdata.obs.set_index("id")
-    adata = read_h5ad(DATA / "debug_OneK1K_cohort_gene_expression_matrix_14_celltypes.h5ad")
+    adata = read_h5ad(
+        DATA / "debug_OneK1K_cohort_gene_expression_matrix_14_celltypes.h5ad"
+    )
     dd = DonorData(adata, gdata, "individual")
     dd.aggregate("X", "Gex")
     assert "Gex" in dd.gdata.obsm
 
+    previous_adata_shape = dd.adata.shape
     dd.aggregate("X", "Gex_CD4NC", filter_key="cell_label", filter_value="CD4 NC")
     assert "Gex_CD4NC" in dd.gdata.obsm
+    assert dd.adata.shape == previous_adata_shape
 
     dd.aggregate("age", "age")
     assert "age" in dd.gdata.obs
