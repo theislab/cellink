95644ceda188362b8b223f6da39e88d30ccd89a1
LArnoldt <github@lucas-arnoldt.de>
2024-10-10 19:24:34 +0200

    tools and preprocessing tests

--- tests/test_tl.py
+++ tests/test_tl.py
@@ -2,17 +2,33 @@ from pathlib import Path
 
 import pytest
 
-def test_():
-    from cellink.tl._simulate_genotype_data import simulate_genotype_data_msprime, simulate_genotype_data_numpy
+from cellink.io import read_sgkit_zarr
+from cellink.tl import simulate_genotype_data_msprime, simulate_genotype_data_numpy, one_hot_encode_genotypes, dosage_per_strand
+import numpy as np
 
-    simulate_genotype_data_msprime(10, 20)
-    geno = simulate_genotype_data_numpy(10, 20)
+DATA = Path("tests/data")
 
-    from cellink.pp._variant_qc import variant_qc
+def test_get_snp_df():
+    print("kskn")
 
-    variant_qc(geno, inplace=False)
+def test_simulate_genotype_data_msprime():
+    adata = simulate_genotype_data_msprime(100, 100)
+    assert adata.shape == (100, 100)
 
-    from cellink.tl._encode_genotype_data import one_hot_encode_genotypes, dosage_per_strand
+def test_simulate_genotype_data_numpy():
+    adata = simulate_genotype_data_numpy(100, 100)
+    assert adata.shape == (100, 100)
 
-    one_hot_encode_genotypes(geno)
-    dosage_per_strand(geno)
+def test_one_hot_encode_genotypes():
+    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
+    gdata.X = gdata.X.compute()
+    one_hot_encode_gdata = one_hot_encode_genotypes(gdata[:100, :100])
+    one_hot_encode_gdata_load = np.load(DATA / "simulated_genotype_calls_one_hot_encoded.npy")
+    assert np.all(one_hot_encode_gdata == one_hot_encode_gdata_load) == True
+
+def test_dosage_per_strand():
+    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
+    gdata.X = gdata.X.compute()
+    dosage_per_strand_gdata = dosage_per_strand(gdata[:100, :100])
+    dosage_per_strand_gdata_load = np.load(DATA / "simulated_genotype_calls_dosage_per_strand.npy")
+    assert np.all(dosage_per_strand_gdata == dosage_per_strand_gdata_load) == True
