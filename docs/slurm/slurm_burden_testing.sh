7d81580e84cbf09f3549487705f526ac6bc9b6a3
Merit Back <merit.back@online.de>
2025-01-14 15:40:27 +0100

    slurm test

--- docs/slurm/slurm_burden_testing.sh
+++ docs/slurm/slurm_burden_testing.sh
@@ -13,8 +13,10 @@
 
 # activate conda env
 source /data/nasif12/modules_if12/SL7/i12g/miniforge/24.9.0-0/etc/profile.d/conda.sh
-conda activate mm_scgenetics
+#conda activate mm_scgenetics
+conda activate mzb_scgenetics
 
 # run burden testing
 echo "Running my SLURM job..."
-python /data/nasif12/home_if12/l_mai/sc-genetics/docs/slurm/run_burden_test.py
+#python /data/nasif12/home_if12/l_mai/sc-genetics/docs/slurm/run_burden_test.py
+python3 ../../src/burden_eval/run_burden_testing.py -c 22 -p "/data/ceph/hdd/project/node_09/sys_gen_students/2024_2025/project04_rare_variant_sc/input_data/filter_vcf_r08/chr22.dose.filtered.R2_0.8.vcz" -o  "/s/project/sys_gen_students/2024_2025/project04_rare_variant_sc/test_output/chr22_all_results_DNA_LM_and_MAF_100k.pkl"
