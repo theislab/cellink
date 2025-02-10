bcf53e86ef9b575b03a67763e2e7f19fa385c84f
Jan Engelmann <jan.engelmann@helmholtz-munich.de>
2025-02-10 11:46:17 +0100

    wip support ellipsis

--- src/cellink/_core/donordata.py
+++ src/cellink/_core/donordata.py
@@ -3,10 +3,15 @@ from __future__ import annotations
 import logging
 from collections.abc import Callable
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 import pandas as pd
 import scanpy as sc
 from anndata import AnnData
+
+if TYPE_CHECKING:
+    from mudata import MuData
+
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
@@ -23,68 +28,71 @@ class DonorData:
     """Store and manage donor-related data with single-cell readouts.
 
     This class allows donor-level, especially genetic, analysis with single-cell datasets.
-    It holds AnnData objects for single-cell (C) and genetic (D) data
+    It holds AnnData objects for donor-level/genetic (G) and single-cell (C) data
 
     Attributes
     ----------
+        G (AnnData): Donor-level AnnData
         C (AnnData): Cell-level AnnData
-        D (AnnData): Donor-level AnnData (e.g.: GenoAnnData)
-        donor_key (str): Key for donor information in C.obs
+        donor_id (str): Key for donor information in C.obs
 
     Returns
     -------
-        _type_: DonorData object
+        DonorData object
     """
 
     def __init__(
         self,
-        C: AnnData,
-        D: AnnData,
-        donor_key: str = DAnn.donor,
+        *,
+        G: AnnData | MuData,
+        C: AnnData | MuData,
+        donor_id: str = DAnn.donor,
         var_dims_to_sync: list[str] = None,
     ):
-        if donor_key not in C.obs.columns:
-            raise ValueError(f"'{donor_key}' not found in C.obs")
-        if donor_key not in D.obs.columns and donor_key != D.obs.index.name:
-            raise ValueError(f"'{donor_key}' must be in gdata.obs or set as index")
-        if donor_key != D.obs.index.name:
-            D.obs = D.obs.set_index(donor_key)
+        if donor_id not in C.obs.columns:
+            raise ValueError(f"'{donor_id}' not found in C.obs")
+        if donor_id not in G.obs.columns and donor_id != G.obs.index.name:
+            raise ValueError(f"'{donor_id}' must be in gdata.obs or set as index")
+        if donor_id != G.obs.index.name:
+            G.obs = G.obs.set_index(donor_id)
 
         self._var_dims_to_sync = [] if var_dims_to_sync is None else var_dims_to_sync
-        self.donor_key = donor_key
-        self._match_donors(C, D)
+        self.donor_id = donor_id
+        self._match_donors(G, C)
 
-    def _match_donors(self, C: AnnData | None = None, D: AnnData | None = None) -> None:
-        if C is None:
-            C = self._C
-        if D is None:
-            D = self._D
+    def _match_donors(self, G: AnnData | MuData, C: AnnData | MuData) -> None:
+        G_idx = G.obs.index
+        C_idx = pd.Index(C.obs[self.donor_id].unique())
+        keep_donors = G_idx.intersection(C_idx)
 
-        # Get unique sample identifiers from both datasets
-        C_idx = pd.Index(C.obs[self.donor_key].unique())
-        D_idx = D.obs.index
-        keep_donors = D_idx.intersection(C_idx)
+        if len(keep_donors) == 0:
+            raise ValueError(
+                (
+                    "No overlapping donors between dd.G and dd.C,"
+                    " make sure dd.C.obs['%s'] exists and corresponds to dd.G.obs_names"
+                ),
+                self.donor_id,
+            )
+        if not keep_donors.equals(G.obs_names):
+            G = G[keep_donors]
 
-        if not keep_donors.equals(D.obs_names):
-            D = D[keep_donors]
-
-        keep_cells = C.obs[self.donor_key].isin(keep_donors)
+        keep_cells = C.obs[self.donor_id].isin(keep_donors)
         if not keep_cells.all():
             C = C[keep_cells]
 
         # Sort cells by donor order
         sorted_cells = C.obs.iloc[
-            pd.Categorical(C.obs[self.donor_key], categories=keep_donors, ordered=True).argsort()
+            pd.Categorical(C.obs[self.donor_id], categories=keep_donors, ordered=True).argsort()
         ].index
         if not sorted_cells.equals(C.obs_names):
             C = C[sorted_cells]
 
         self._C = C
-        self._D = D
+        self._G = G
 
     def copy(self) -> DonorData:
         self._C = self._C.copy()
-        self._D = self._D.copy()
+        self._G = self._G.copy()
         return self
 
     @property
@@ -92,44 +100,73 @@ class DonorData:
         return self._C
 
     @C.setter
-    def C(self, value: AnnData) -> None:
-        if not isinstance(value, AnnData):
-            raise ValueError("C must be an AnnData object")
+    def C(self, value: AnnData | MuData) -> None:
+        if not isinstance(value, AnnData | MuData):
+            raise ValueError("C must be an AnnData or MuData object")
         self._C = value
-        self._match_donors()
+        self._match_donors(self._G, self._C)
 
     @property
-    def D(self) -> AnnData:
-        return self._D
+    def G(self) -> AnnData:
+        return self._G
 
-    @D.setter
-    def D(self, value: AnnData) -> None:
-        if not isinstance(value, AnnData):
-            raise ValueError("D must be an AnnData object")
-        self._D = value
-        self._match_donors()
+    @G.setter
+    def G(self, value: AnnData | MuData) -> None:
+        if not isinstance(value, AnnData | MuData):
+            raise ValueError("G must be an AnnData or MuData object")
+        self._G = value
+        self._match_donors(self._G, self._C)
 
+    def _sync_var_dims(self, G: AnnData | MuData, C: AnnData | MuData) -> None:
+        """Sync _D.obsm if needed, drop deleted keys from _var_dims_to_sync"""
+        keys_to_sync = []
+        for v in self._var_dims_to_sync:
+            if v in G.obsm:
+                keys_to_sync.append(v)
+                G.obsm[v] = G.obsm[v].loc[:, C.var.index]
+        self._var_dims_to_sync = keys_to_sync
+
+    def sel(
+        self,
+        *,
+        G_obs: slice = slice(None),
+        G_var: slice = slice(None),
+        C_obs: slice = slice(None),
+        C_var: slice = slice(None),
+    ):
+        _G = self.G[G_obs]
+        _G = _G[:, G_var]
+        _C = self.C[C_obs]
+        _C = _C[:, C_var]
+
+        self._sync_var_dims(_G, _C)
+        return DonorData(G=_G, C=_C, donor_id=self.donor_id, var_dims_to_sync=self._var_dims_to_sync)
+
+    # TODO: properly support ellipsis
     def __getitem__(self, key):
-        key = key if isinstance(key, tuple) else (key,)
-        if len(key) > 4:
-            raise ValueError(f"Invalid slice length: {len(key)}")
+        if not isinstance(key, tuple):
+            key = (key,)
+        # Replace any Ellipsis with slice(None)
+        key = tuple(slice(None) if k is Ellipsis else k for idx, k in enumerate(key))
+        _key = []
 
-        _D = self.D[key[0]]
-        _C = self.C
-        if len(key) >= 2:
-            _D = _D[:, key[1]]
-        if len(key) >= 3:
-            _C = _C[key[2]]
-        if len(key) == 4:
-            _C = _C[:, key[3]]
-            keys_to_sync = []
-            for key in self._var_dims_to_sync:
-                if key in _D.obsm:
-                    keys_to_sync.append(key)
-                    _D.obsm[key] = _D.obsm[key].loc[:, _C.var.index]
-            self._var_dims_to_sync = keys_to_sync
+        # Pad to 4-tuple: (D_obs, D_var, C_obs, C_var)
+        key = key + (slice(None),) * (4 - len(key))
+        key = tuple(
+            (idx,) if isinstance(idx, str) else idx for idx in key
+        )  # needed because Mudata[str] looks up modalities
+        _G = self.G[key[0]]
+        _G = _G[:, key[1]]
+        _C = self.C[key[2]]
+        _C = _C[:, key[3]]
 
-        return DonorData(_C, _D, self.donor_key, self._var_dims_to_sync)
+        self._sync_var_dims(_G, _C)
+        return DonorData(
+            G=_G,
+            C=_C,
+            donor_id=self.donor_id,
+            var_dims_to_sync=self._var_dims_to_sync,
+        )
 
     def aggregate(
         self,
@@ -182,16 +219,16 @@ class DonorData:
 
         if obs is not None:
             obs = obs if isinstance(obs, list) else [obs]
-            _data = adata.obs.groupby(self.donor_key, observed=True)[obs].agg(func)
+            _data = adata.obs.groupby(self.donor_id, observed=True)[obs].agg(func)
             dtype = _data.dtypes.iloc[0]
-            data = pd.DataFrame(index=self.D.obs_names, columns=obs, dtype=dtype)
+            data = pd.DataFrame(index=self.G.obs_names, columns=obs, dtype=dtype)
             data.loc[data.index, obs] = _data
             if add_to_obs:
-                self.D.obs.loc[data.index, obs] = data
+                self.G.obs.loc[data.index, obs] = data
             else:
-                self.D.obsm[key_added] = data
+                self.G.obsm[key_added] = data
         else:
-            aggdata = sc.get.aggregate(adata, by=self.donor_key, func=func, layer=layer, obsm=obsm)
+            aggdata = sc.get.aggregate(adata, by=self.donor_id, func=func, layer=layer, obsm=obsm)
             if slot == "obsm":
                 # use columns of obsm dataframe if provided
                 columns = getattr(getattr(adata, slot), "columns", None)
@@ -201,15 +238,15 @@ class DonorData:
                     self._var_dims_to_sync.append(key_added)
 
             dtype = aggdata.layers[func].dtype
-            data = pd.DataFrame(index=self.D.obs_names, columns=columns, dtype=dtype)
+            data = pd.DataFrame(index=self.G.obs_names, columns=columns, dtype=dtype)
             data.loc[aggdata.obs_names] = aggdata.layers[func]
-            self.D.obsm[key_added] = data
+            self.G.obsm[key_added] = data
 
     def prep_repr(self) -> str:
-        """String representation of DonorData showing side-by-side adata and gdata views."""
+        """String representation of DonorData showing side-by-side dd.G and dd.C views."""
         # Split the representations into lines
-        D_lines, D_highlight = _anndata_repr(self.D, self.D.n_obs, self.D.n_vars, self._var_dims_to_sync)
-        C_lines, C_highlight = _anndata_repr(self.C, self.C.n_obs, self.C.n_vars, [self.donor_key])
+        G_lines, G_highlight = _anndata_repr(self.G, self.G.n_obs, self.G.n_vars, self._var_dims_to_sync)
+        C_lines, C_highlight = _anndata_repr(self.C, self.C.n_obs, self.C.n_vars, [self.donor_id])
 
         def pad_lists(l1, l2):
             max_lines = max(len(l1), len(l2))
@@ -218,8 +255,8 @@ class DonorData:
             return l1, l2
 
         # Ensure both have the same number of lines by padding with empty lines if necessary
-        D_lines, C_lines = pad_lists(D_lines, C_lines)
-        D_highlight, C_highlight = pad_lists(D_highlight, C_highlight)
+        G_lines, C_lines = pad_lists(G_lines, C_lines)
+        G_highlight, C_highlight = pad_lists(G_highlight, C_highlight)
 
         def highlight_lines(lines, highlights):
             _lines = []
@@ -231,14 +268,14 @@ class DonorData:
                 _lines.append(text)
             return _lines
 
+        G_lines = highlight_lines(G_lines, G_highlight)
         C_lines = highlight_lines(C_lines, C_highlight)
-        D_lines = highlight_lines(D_lines, D_highlight)
 
         # Create table
         table = Table(show_header=True, header_style=HIGHLIGHT_COLOR)
-        table.add_column("D (donors)", max_width=50, justify="left")
+        table.add_column("G (donors)", max_width=50, justify="left")
         table.add_column("C (cells)", max_width=50, justify="left")
-        for adata_line, gdata_line in zip(C_lines, D_lines, strict=True):
+        for gdata_line, adata_line in zip(G_lines, C_lines, strict=True):
             table.add_row(gdata_line, adata_line)
 
         return table
@@ -250,11 +287,15 @@ class DonorData:
 
     def __str__(self) -> str:
         n_donors, n_donor_vars, n_cells, n_cell_vars = self.shape
-        return f"{__class__.__name__}({n_donors=}, {n_donor_vars=}, {n_cells=}, {n_cell_vars=})"
+        return (
+            f"{self.__class__.__name__}"
+            f"(n_G_obs={n_donors:,}, n_G_vars={n_donor_vars:,}, "
+            f"n_C_obs={n_cells:,}, n_C_vars={n_cell_vars:,})"
+        )
 
     @property
     def shape(self) -> tuple[tuple[int, int], tuple[int, int]]:
-        return *self.D.shape, *self.C.shape
+        return *self.G.shape, *self.C.shape
 
 
 def _anndata_repr(adata, n_obs, n_vars, highlight_keys=None) -> str:
@@ -262,7 +303,7 @@ def _anndata_repr(adata, n_obs, n_vars, highlight_keys=None) -> str:
         backed_at = f" backed at {str(adata.filename)!r}"
     else:
         backed_at = ""
-    lines = [[f"AnnData object with n_obs × n_vars = {n_obs} × {n_vars}{backed_at}"]]
+    lines = [[f"AnnData object with n_obs × n_vars = {n_obs:,} × {n_vars:,} {backed_at}"]]
     highlight = [[False]]
     for attr in ["obs", "var", "uns", "obsm", "varm", "layers", "obsp", "varp"]:
         keys = getattr(adata, attr).keys()
