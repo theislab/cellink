fe108fe117ddd88034ce785a6412f39ee3a73045
Eva Holtkamp <eva.holtkamp@gmx.de>
2025-10-29 20:00:08 +0100

    fix import so docs render

--- src/cellink/tl/__init__.py
+++ src/cellink/tl/__init__.py
@@ -1,3 +1,5 @@
+# from .external import calculate_ld, calculate_pcs, run_jaxqtl, run_mixmil, run_tensorqtl
+from . import external
 from ._annotate_snps_ensembl_rest import get_snp_df
 from ._annotate_snps_genotype_data import (
     add_vep_annos_to_gdata,
@@ -9,6 +11,5 @@ from ._annotate_snps_genotype_data import (
 )
 from ._encode_genotype_data import dosage_per_strand, one_hot_encode_genotypes
 
-# from .external import calculate_ld, calculate_pcs, run_jaxqtl, run_mixmil, run_tensorqtl
 # TODO mixmil requires torch which we do not want to install by default.
 from ._rvat import beta_weighting, run_burden_test, run_skat_test
