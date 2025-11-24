a71d793f4402285e72ca924c5b0028891987e3db
Lucas Arnoldt <lucas.arnoldt@helmholtz-munich.de>
2025-11-24 18:36:44 +0100

    Refactor tests

--- tests/test_tl.py
+++ tests/test_tl.py
@@ -12,29 +12,11 @@ from cellink.tl import (
     add_vep_annos_to_gdata,
     aggregate_annotations_for_varm,
     combine_annotations,
-    dosage_per_strand,
-    one_hot_encode_genotypes,
     run_vep,
 )
 
 DATA = Path("tests/data")
 CONFIG = Path("configs")
-
-
-def test_one_hot_encode_genotypes():
-    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
-    gdata.X = gdata.X.compute()
-    one_hot_encode_gdata = one_hot_encode_genotypes(gdata[:100, :100])
-    one_hot_encode_gdata_load = np.load(DATA / "simulated_genotype_calls_one_hot_encoded.npy")
-    assert np.allclose(one_hot_encode_gdata, one_hot_encode_gdata_load)
-
-
-def test_dosage_per_strand():
-    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
-    gdata.X = gdata.X.compute()
-    dosage_per_strand_gdata = dosage_per_strand(gdata[:100, :100])
-    dosage_per_strand_gdata_load = np.load(DATA / "simulated_genotype_calls_dosage_per_strand.npy")
-    assert np.allclose(dosage_per_strand_gdata, dosage_per_strand_gdata_load)
 
 
 @pytest.fixture
---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from cellink.io import read_sgkit_zarr
+from cellink.utils import (
+    dosage_per_strand,
+    one_hot_encode_genotypes,
+)
+
+DATA = Path("tests/data")
+CONFIG = Path("configs")
+
+
+def test_one_hot_encode_genotypes():
+    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
+    gdata.X = gdata.X.compute()
+    one_hot_encode_gdata = one_hot_encode_genotypes(gdata[:100, :100])
+    one_hot_encode_gdata_load = np.load(DATA / "simulated_genotype_calls_one_hot_encoded.npy")
+    assert np.allclose(one_hot_encode_gdata, one_hot_encode_gdata_load)
+
+
+def test_dosage_per_strand():
+    gdata = read_sgkit_zarr(DATA / "simulated_genotype_calls.vcz")
+    gdata.X = gdata.X.compute()
+    dosage_per_strand_gdata = dosage_per_strand(gdata[:100, :100])
+    dosage_per_strand_gdata_load = np.load(DATA / "simulated_genotype_calls_dosage_per_strand.npy")
+    assert np.allclose(dosage_per_strand_gdata, dosage_per_strand_gdata_load)
